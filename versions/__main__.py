0cc253f66d83a85d0cc90ad5cf9857b04dfb3089
Matt Turner <matturner@gmail.com>
2018-06-02 16:52:07 +0100

    Change list of things fetched

--- versions/__main__.py
+++ versions/__main__.py
@@ -2,12 +2,10 @@
 
 import versions
 
+versions.github_releases_groupby_maj_min("kubernetes", "kubernetes", 2, 3)
 versions.github_releases_prerelease_ga("envoyproxy", "envoy")
 versions.github_releases_prerelease_ga("istio", "istio")
-versions.github_releases_prerelease_ga("runconduit", "conduit")
-versions.github_releases_groupby_maj_min("kubernetes", "kubernetes", 2, 3)
 versions.github_releases_prerelease_ga("linkerd", "linkerd")
-versions.github_releases_prerelease_ga("linkerd", "linkerd-tcp")
+versions.github_releases_prerelease_ga("runconduit", "conduit")
 versions.kernel_org_mainline_stable()
 versions.github_releases_prerelease_ga("zfsonlinux", "zfs")
-versions.github_releases_prerelease_ga("zfsonlinux", "spl")
