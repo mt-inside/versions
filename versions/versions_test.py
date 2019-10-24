e42abe3f8a0ce58eae1c29e085fe33eb0e4b3625
Matt Turner <matturner@gmail.com>
2019-10-24 17:01:08 +0200

    sssh flake8

--- versions/versions_test.py
+++ versions/versions_test.py
@@ -1,13 +1,13 @@
 import json
 
-from versions import *
+import versions
 
 
 def test_parse_gh_release_k8s():
     with open('testdata/k8s.json') as t:
         rs = json.load(t)
 
-    a = parse_gh_release(rs, 2, 3)
+    a = versions.parse_gh_release(rs, 2, 3)
 
     assert len(a) == 3
     assert type(a[0]) == dict
@@ -25,7 +25,7 @@ def test_parse_gh_release_zfs():
     with open('testdata/zfs.json') as t:
         rs = json.load(t)
 
-    a = parse_gh_release(rs, 2, 2)
+    a = versions.parse_gh_release(rs, 2, 2)
 
     assert len(a) == 2
     assert type(a[0]) == dict
@@ -40,7 +40,7 @@ def test_parse_gh_release_zfs():
 
 
 def test_ver_num():
-    assert get_ver_num("1.2.3") == "1.2.3"
-    assert get_ver_num("v1.2.3") == "1.2.3"
-    assert get_ver_num("1.2.3-0") == "1.2.3-0"
-    assert get_ver_num("1.2.3-rc1.beta1") == "1.2.3-rc1.beta1"
+    assert versions.get_ver_num("1.2.3") == "1.2.3"
+    assert versions.get_ver_num("v1.2.3") == "1.2.3"
+    assert versions.get_ver_num("1.2.3-0") == "1.2.3-0"
+    assert versions.get_ver_num("1.2.3-rc1.beta1") == "1.2.3-rc1.beta1"
